2993aef98aca5b0ab78841277aa5d0f62d684a84
Sion Smith <sion.smith@gmail.com>
2022-10-27 09:09:56 +0100

    templated topology file using Python

--- scripts/julieops_config_generator.py
+++ scripts/julieops_config_generator.py
@@ -4,7 +4,7 @@ import jinja2
 
 def generate_content():
     """
-    Use jinja2 template to generate connector content
+    Use jinja2 template to generate topology content
     """
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
                              trim_blocks=True)
@@ -17,7 +17,10 @@ def generate_content():
         SCHEMA_REGISTRY_URL=args.schema_registry_url,
         SCHEMA_REGISTRY_ID=args.schema_registry_id,
         CONNECT_CLUSTER_ID=args.connect_cluster_id,
-        KSQL_CLUSTER_ID=args.ksql_cluster_id
+        KSQL_CLUSTER_ID=args.ksql_cluster_id,
+        TENANT_STATE_TOPIC_NAME=args.tenant_state_topic,
+        MANAGED_PREFIXES=args.managed_topics_list,
+        ROLES_FILE=args.custom_roles_file
     )
 
     print(export)
@@ -74,6 +77,25 @@ if __name__ == "__main__":
         type=str,
         default='ksql-cluster',
         help='Ksql cluster id',
+    )
+    parser.add_argument(
+        '--tenant-state-topic',
+        type=str,
+        required=True,
+        help='Tenant state topic name',
+    )
+    parser.add_argument(
+        '--managed-topics-list',
+        nargs='+',
+        required=True,
+        default=[],
+        help='List of topics for JulieOps to manage'
+    )
+    parser.add_argument(
+        '--custom-roles-file',
+        type=str,
+        default='custom-roles/role-bindings.yaml',
+        help='Filename of custom roles'
     )
     args = parser.parse_args()
     generate_content()
